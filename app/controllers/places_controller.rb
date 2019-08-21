a3707e0d660b57dcc2b3517448d42ab60ada35ab
ajrudder <adam.rudder@gmail.com>
2019-08-21 09:19:42 -0700

    destroy completed

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -9,10 +9,29 @@ class PlacesController < ApplicationController
 		@places = Place.new
 	end
 
-
 #The place_params part is what pulls the values of name, description and address from the place form. Then the Place.create is what actually sends the item to the database.
 	def create
 		current_user.places.create(place_params)
+		redirect_to root_path
+	end
+
+	def show
+		@place = Place.find(params[:id])
+	end
+
+	def edit
+		@place = Place.find(params[:id])
+	end
+
+	def update
+		@place = Place.find(params[:id])
+		@place.update_attributes(place_params)
+		redirect_to root_path
+	end
+
+	def destroy
+		@place = Place.find(params[:id])
+		@place.destroy
 		redirect_to root_path
 	end
 	
